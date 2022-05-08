4456ce8eb4dba206a50145e79a7660bfd60872d9
gabrielseron <gabriel.seron.pro@gmail.com>
2022-05-08 23:04:10 +0200

    Add formsModule / bind ngModel

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  defaultWidth = 50;
+  defaultHeight = 50;
+
   constructor() { }
 
   ngOnInit(): void {
+    let w  = localStorage.getItem('width');
+    let h  = localStorage.getItem('height');
+
+    if (w !== undefined && typeof w === 'string' && w !=='0')
+      this.defaultWidth = parseInt(w);
+
+    if (h !== undefined && typeof h === 'string' && w !=='0')
+      this.defaultHeight = parseInt(h);
   }
 
 }
--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -7,12 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  width = 50;
-  height = 50;
+  defaultWidth = 50;
+  defaultHeight = 50;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    let w  = localStorage.getItem('width');
+    let h  = localStorage.getItem('height');
+
+    if (w !== undefined && typeof w === 'string' && w !=='0')
+      this.defaultWidth = parseInt(w);
+
+    if (h !== undefined && typeof h === 'string' && w !=='0')
+      this.defaultHeight = parseInt(h);
+  }
+
+  save() {
+    localStorage.setItem('width', JSON.stringify(this.defaultWidth));
+    localStorage.setItem('height', JSON.stringify(this.defaultHeight));
+    this.router.navigateByUrl('/game')
+  }
+
+  reset() {
+    this.defaultWidth = 50;
+    this.defaultHeight = 50;
+    localStorage.clear();
+    this.router.navigateByUrl('/game')
   }
 
 }
